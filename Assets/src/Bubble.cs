09073a062f8a225ac67104a1da2bf299115f9a54
KyleHarsh <112901557+KyleHarsh@users.noreply.github.com>
2025-03-27 18:02:10 -0400

    Created bubble and particle classes

---
+++ Assets/src/Bubble.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bubble : MonoBehaviour
+{
+    void spawnParticles(int num, UnityEngine.Vector3 center)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            UnityEngine.Vector3 pos = Random.onUnitSphere*this.radius;
+            SurfaceParticle particle = new SurfaceParticle(center + pos, UnityEngine.Vector3.zero, UnityEngine.Vector3.zero);
+            this.surfaceParticles.Add(particle);
+        }
+        for (int i = 0; i < surfaceParticles.Count; ++i)
+        {
+            for(int j = 0; j < surfaceParticles.Count; ++j)
+            {
+                if (i == j) break;
+                if ((surfaceParticles[i].getPosition() - surfaceParticles[j].getPosition()).magnitude <= neighborDist)
+                {
+                    surfaceParticles[i].addNeighbor(surfaceParticles[j]);
+                    surfaceParticles[j].addNeighbor(surfaceParticles[i]);
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    UnityEngine.Vector3 calcCenterOfMass()
+    {
+        //not yet implemented
+        return UnityEngine.Vector3.zero;
+    }
+
+    private static readonly float neighborDist = 0.1f;
+
+    List<SurfaceParticle> surfaceParticles;
+    float radius;
+}
---
+++ Assets/src/SurfaceParticle.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class SurfaceParticle : MonoBehaviour
+{
+
+    public SurfaceParticle(UnityEngine.Vector3 p, UnityEngine.Vector3 v, UnityEngine.Vector3 a, float m = 0.001f)
+    {
+        this.position = p;
+        this.velocity = v;
+        this.acceleration = a;
+        this.mass = m;
+        this.neigbors = new List<SurfaceParticle>();
+    }
+
+    public void addNeighbor(SurfaceParticle n)
+    {
+        this.neigbors.Add(n);
+    }
+
+    public UnityEngine.Vector3 getPosition() { return this.position; }
+    public void externalForce(UnityEngine.Vector3 f)
+    {
+        acceleration += f * this.mass;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position += velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+
+        // surface tension kinda
+        // particles are assumed to be 0.1 unit away from each other by default
+        for (int i = 0; i < neigbors.Count; i++)
+        {
+            UnityEngine.Vector3 direction = (neigbors[i].position - this.position).normalized;
+            acceleration -= k * ((neigbors[i].position - this.position).magnitude - partDist) * direction;
+        }
+        acceleration -= acceleration * damping * Time.deltaTime;
+    }
+
+    private static readonly float k = 0.1f;
+    private static readonly float partDist = 0.1f;
+    private static readonly float damping = 0.1f; //this is the ammount of damping/sec
+
+    //representation:
+    float mass;
+    UnityEngine.Vector3 position;
+    UnityEngine.Vector3 velocity;
+    UnityEngine.Vector3 acceleration;
+
+    List<SurfaceParticle> neigbors;
+}
